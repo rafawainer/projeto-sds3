37a6364df85d0aa066d16e73391446ed8851a808
Rafael Wainer <rafael.wainer@gmail.com>
2021-05-08 19:59:26 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/rafawainer" target="_blank" rel="noreferrer">Rafael Wainer</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Desenvolvimento realizado por: <a href="https://instagram.com/santistademoto" target="_blank" rel="noreferrer">@santistademoto</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
